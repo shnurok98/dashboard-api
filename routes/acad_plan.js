3bdf2212146709a1cf02c694c62e689f0f25befc
shnurok98 <shnurok-98@mail.ru>
2020-07-15 16:59:58 +0300

    final dump, final doc

--- routes/acad_plan.js
+++ routes/acad_plan.js
@@ -6,148 +6,149 @@ const AcadPlan = require('../models/acad_plan');
 const Upload = require('../models/upload');
 const { prefix } = require('../config').api;
 
-router.use('*', async (req, res, next) => {
-	try {
-		const url = req.baseUrl.replace(prefix, '');
-		const partsUrl = url.match(/\/\w+/g); // все части вида '/word'
-		// console.log(partsUrl);
-		const parentUrl = partsUrl[0];
-		let resourceId = partsUrl[1];
-		if ( resourceId ) resourceId = resourceId.substring(1, resourceId.length);
-		
-		// система прав
-		const access = await Access(req.user, req.method, parentUrl, resourceId);
-		if ( !access ) return res.sendStatus(403);
+router.use('*', async(req, res, next) => {
+    try {
+        const url = req.baseUrl.replace(prefix, '');
+        const partsUrl = url.match(/\/\w+/g); // все части вида '/word'
+        // console.log(partsUrl);
+        const parentUrl = partsUrl[0];
+        let resourceId = partsUrl[1];
+        if (resourceId) resourceId = resourceId.substring(1, resourceId.length);
 
-		next();
-	} catch (e) {
-		console.error(e);
-		res.sendStatus(500);
-  }
+        // система прав
+        const access = await Access(req.user, req.method, parentUrl, resourceId);
+        if (!access) return res.sendStatus(403);
+
+        next();
+    } catch (e) {
+        console.error(e);
+        res.sendStatus(500);
+    }
 })
 
 router.get('/', (req, res) => {
-  AcadPlan.getAll(req.query, (err, acad) => {
-    if (err) return res.status(400).json(err);
-		if (acad) {
-			res.status(200).json(acad);
-		}else{
-			res.sendStatus(500);
-		}
-  });
+    AcadPlan.getAll(req.query, (err, acad) => {
+        if (err) return res.status(400).json(err);
+        if (acad) {
+            res.status(200).json(acad);
+        } else {
+            res.sendStatus(500);
+        }
+    });
 });
 
 router.get('/:id', (req, res) => {
-  AcadPlan.get(req.params.id, (err, acad) => {
-		if (err) console.error(err);
-		if (acad) {
-			res.status(200).json(acad);
-		}else{
-			res.sendStatus(404);
-		}
-	});
+    AcadPlan.get(req.params.id, (err, acad) => {
+        if (err) console.error(err);
+        if (acad) {
+            res.status(200).json(acad);
+        } else {
+            res.sendStatus(404);
+        }
+    });
 });
 
 router.get('/:id/files', (req, res) => {
-  Upload.getList('files_acad_plan', +req.params.id, req.query, (err, rows) => {
-		if (err) return res.status(400).json(err);
-		if (rows) {
-			res.status(200).json(rows);
-		} else {
-			res.sendStatus(500);
-		}
-	});
+    Upload.getList('files_acad_plan', +req.params.id, req.query, (err, rows) => {
+        if (err) return res.status(400).json(err);
+        if (rows) {
+            res.status(200).json(rows);
+        } else {
+            res.sendStatus(500);
+        }
+    });
 });
 
 router.get('/:id/semester/:num', (req, res) => {
-  AcadPlan.getSemester(+req.params.id, +req.params.num, (err, dis) => {
-    if (err) console.error(err);
-		if (dis) {
-			res.status(200).json(dis);
-		}else{
-			res.sendStatus(404);
-		}
-  })
-});
-
-router.put('/discipline/:id', async (req, res) => {
-  try {
-    AcadPlan.updateDiscipline(+req.params.id, req.body, (err, id) => {
-      if ( err ) return res.status(400).json(err);
-			if ( id ) {
-				res.sendStatus(204)
-			} else {
-				res.sendStatus(404)
-			}
+    AcadPlan.getSemester(+req.params.id, +req.params.num, (err, dis) => {
+        if (err) console.error(err);
+        if (dis) {
+            res.status(200).json(dis);
+        } else {
+            res.sendStatus(404);
+        }
     })
-    
-  } catch (e) {
-    console.error(e);
-    res.sendStatus(500);
-  }
 });
 
-router.post('/', async (req, res) => {
-  try {
-    const data = req.body;
-		const acadplan = new AcadPlan(data);
-		acadplan.save((err, id) => {
-			if ( err ) return res.status(400).json(err);
-			if ( id ) {
-				res.location(`/api/acad_plan/${id}`);
-				res.sendStatus(201)
-			} 
-		});
-  } catch(e) {
-    console.error(e);
-    res.sendStatus(500);
-  }
+router.put('/discipline/:id', async(req, res) => {
+    try {
+        AcadPlan.updateDiscipline(+req.params.id, req.body, (err, id) => {
+            if (err) return res.status(400).json(err);
+            if (id) {
+                res.sendStatus(204)
+            } else {
+                res.sendStatus(404)
+            }
+        })
+
+    } catch (e) {
+        console.error(e);
+        res.sendStatus(500);
+    }
+});
+
+router.post('/', async(req, res) => {
+    try {
+        const data = req.body;
+        const acadplan = new AcadPlan(data);
+        acadplan.save((err, id) => {
+            if (err) return res.status(400).json(err);
+            if (id) {
+                res.location(`/api/acad_plan/${id}`);
+                res.status(201).json({ id: id });
+                // res.sendStatus(201)
+            }
+        });
+    } catch (e) {
+        console.error(e);
+        res.sendStatus(500);
+    }
 });
 
 // Block, Part, Module
 
 router.get('/:table(module|block|part)/:id', (req, res) => {
-  const table = 'acad_' + req.params.table;
-
-  AcadPlan.getDir(table, +req.params.id, (err, dir) => {
-    if ( err ) return res.status(400).json(err);
-    if ( dir ) {
-      res.status(200).json(dir);
-    } else {
-      res.sendStatus(404);
-    }
-  })
-});
-
-router.put('/:table(module|block|part)/:id', async (req, res) => {
-  try {
     const table = 'acad_' + req.params.table;
 
-    AcadPlan.updateDir(table, +req.params.id, req.body, (err, dir) => {
-      if ( err ) return res.status(400).json(err);
-      if ( dir ) {
-        res.sendStatus(204)
-      } else {
-        res.sendStatus(404);
-      }
+    AcadPlan.getDir(table, +req.params.id, (err, dir) => {
+        if (err) return res.status(400).json(err);
+        if (dir) {
+            res.status(200).json(dir);
+        } else {
+            res.sendStatus(404);
+        }
     })
-    
-  } catch (e) {
-    console.error(e);
-    res.sendStatus(500);
-  }
+});
+
+router.put('/:table(module|block|part)/:id', async(req, res) => {
+    try {
+        const table = 'acad_' + req.params.table;
+
+        AcadPlan.updateDir(table, +req.params.id, req.body, (err, dir) => {
+            if (err) return res.status(400).json(err);
+            if (dir) {
+                res.sendStatus(204)
+            } else {
+                res.sendStatus(404);
+            }
+        })
+
+    } catch (e) {
+        console.error(e);
+        res.sendStatus(500);
+    }
 })
 
-router.delete('/:id', async (req, res) => {
-	try {
-		AcadPlan.delete(+req.params.id, (err) => {
-			if ( err ) return res.status(409).json(err);
-			res.sendStatus(204);
-		})
-	} catch (e) {
-		console.error(e);
-		res.sendStatus(500);
-  }
+router.delete('/:id', async(req, res) => {
+    try {
+        AcadPlan.delete(+req.params.id, (err) => {
+            if (err) return res.status(409).json(err);
+            res.sendStatus(204);
+        })
+    } catch (e) {
+        console.error(e);
+        res.sendStatus(500);
+    }
 });
 
 module.exports = router;
--- routes/dep_load.js
+++ routes/dep_load.js
@@ -6,117 +6,118 @@ const DepLoad = require('../models/dep_load');
 const Upload = require('../models/upload');
 const { prefix } = require('../config').api;
 
-router.use('*', async (req, res, next) => {
-	try {
-		const url = req.baseUrl.replace(prefix, '');
-		const partsUrl = url.match(/\/\w+/g); // все части вида '/word'
-		// console.log(partsUrl);
-		const parentUrl = partsUrl[0];
-		let resourceId = partsUrl[1];
-		if ( resourceId ) resourceId = resourceId.substring(1, resourceId.length);
-		
-		// система прав
-		const access = await Access(req.user, req.method, parentUrl, resourceId);
-		if ( !access ) return res.sendStatus(403);
+router.use('*', async(req, res, next) => {
+    try {
+        const url = req.baseUrl.replace(prefix, '');
+        const partsUrl = url.match(/\/\w+/g); // все части вида '/word'
+        // console.log(partsUrl);
+        const parentUrl = partsUrl[0];
+        let resourceId = partsUrl[1];
+        if (resourceId) resourceId = resourceId.substring(1, resourceId.length);
 
-		next();
-	} catch (e) {
-		console.error(e);
-		res.sendStatus(500);
-  }
+        // система прав
+        const access = await Access(req.user, req.method, parentUrl, resourceId);
+        if (!access) return res.sendStatus(403);
+
+        next();
+    } catch (e) {
+        console.error(e);
+        res.sendStatus(500);
+    }
 })
 
 router.get('/', (req, res) => {
-	DepLoad.getAll(req.query, (err, dep) => {
-    if (err) return res.status(400).json(err);
-		if (dep) {
-			res.status(200).json(dep);
-		}else{
-			res.sendStatus(500);
-		}
-  });
+    DepLoad.getAll(req.query, (err, dep) => {
+        if (err) return res.status(400).json(err);
+        if (dep) {
+            res.status(200).json(dep);
+        } else {
+            res.sendStatus(500);
+        }
+    });
 });
 
 router.get('/:id', (req, res) => {
-	DepLoad.get(req.params.id, (err, depload) => {
-		if (err) console.error(err);
-		if (depload) {
-			res.status(200).json(depload);
-		}else{
-			res.sendStatus(404);
-		}
-	});
+    DepLoad.get(req.params.id, (err, depload) => {
+        if (err) console.error(err);
+        if (depload) {
+            res.status(200).json(depload);
+        } else {
+            res.sendStatus(404);
+        }
+    });
 });
 
 router.get('/:id/files', (req, res) => {
-  Upload.getList('files_dep_load', +req.params.id, req.query, (err, rows) => {
-		if (err) return res.status(400).json(err);
-		if (rows) {
-			res.status(200).json(rows);
-		} else {
-			res.sendStatus(500);
-		}
-	});
+    Upload.getList('files_dep_load', +req.params.id, req.query, (err, rows) => {
+        if (err) return res.status(400).json(err);
+        if (rows) {
+            res.status(200).json(rows);
+        } else {
+            res.sendStatus(500);
+        }
+    });
 });
 
-router.post('/', async (req, res) => {
-  try {
-    const data = req.body;
-		const depLoad = new DepLoad(data);
-		depLoad.save((err, id) => {
-			if ( err ) return res.status(400).json(err);
-			if ( id ) {
-				res.location(`/api/dep_load/${id}`);
-				res.sendStatus(201)
-			} 
-		});
-  } catch(e) {
-    console.error(e);
-    res.sendStatus(500);
-  }
+router.post('/', async(req, res) => {
+    try {
+        const data = req.body;
+        const depLoad = new DepLoad(data);
+        depLoad.save((err, id) => {
+            if (err) return res.status(400).json(err);
+            if (id) {
+                res.location(`/api/dep_load/${id}`);
+                res.status(201).json({ id: id });
+                res.sendStatus(201)
+            }
+        });
+    } catch (e) {
+        console.error(e);
+        res.sendStatus(500);
+    }
 });
 
-router.put('/discipline/:discipline_id', async (req, res) => {
-  try {
-    DepLoad.updateDiscipline(+req.params.discipline_id, req.body, (err, id) => {
-      if ( err ) return res.status(400).json(err);
-			if ( id ) {
-				res.sendStatus(204)
-			} else {
-				res.sendStatus(404)
-			}
-    })
-    
-  } catch (e) {
-		console.error(e);
-    res.sendStatus(500);
-  }
+router.put('/discipline/:discipline_id', async(req, res) => {
+    try {
+        DepLoad.updateDiscipline(+req.params.discipline_id, req.body, (err, id) => {
+            if (err) return res.status(400).json(err);
+            if (id) {
+                res.sendStatus(204)
+            } else {
+                res.sendStatus(404)
+            }
+        })
+
+    } catch (e) {
+        console.error(e);
+        res.sendStatus(500);
+    }
 });
 
-router.post('/discipline/teacher', async (req, res) => {
-  try {
-    DepLoad.setTeacher(req.body, (err, id) => {
-			if ( err ) return res.status(400).json(err);
-			if ( id ) {
-				return res.sendStatus(200);
-			}
-		})
-  } catch(e) {
-    console.error(e);
-    res.sendStatus(500);
-  }
+router.post('/discipline/teacher', async(req, res) => {
+    try {
+        DepLoad.setTeacher(req.body, (err, id) => {
+            if (err) return res.status(400).json(err);
+            if (id) {
+                return res.sendStatus(200);
+            }
+        })
+    } catch (e) {
+        console.error(e);
+        res.sendStatus(500);
+    }
 });
 
-router.delete('/:id', async (req, res) => {
-	try {
-		DepLoad.delete(+req.params.id, (err) => {
-			if ( err ) return res.status(409).json(err);
-			res.sendStatus(204);
-		})
-	} catch (e) {
-		console.error(e);
-		res.sendStatus(500);
-  }
+router.delete('/:id', async(req, res) => {
+    try {
+        DepLoad.delete(+req.params.id, (err) => {
+            if (err) return res.status(409).json(err);
+            res.sendStatus(204);
+        })
+    } catch (e) {
+        console.error(e);
+        res.sendStatus(500);
+    }
 });
 
 module.exports = router;
